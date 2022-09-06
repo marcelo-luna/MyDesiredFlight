ca420066090ee4696e8a6603377ae98fdc846e63
Marcelo Luna <marcelo.luna@outlook.com>
2022-09-06 23:44:26 +0200

    Add chrome and chrome driver in docker file

--- source/MyDesiredFlight.Bll/AirLines/Latam.cs
+++ source/MyDesiredFlight.Bll/AirLines/Latam.cs
@@ -21,19 +21,57 @@ namespace MyDesiredFlight.Bll.AirLines
 
         public async Task<decimal> SearchFly(string origin, string destination, string dateFrom, string dateTo, string[] options)
         {
-            using (var driver = new ChromeDriver())
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("--headless", "--disable-gpu", "--no-sandbox", "--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36");
+            var service = ChromeDriverService.CreateDefaultService("/usr/bin/", "chromedriver");
+            //var service = ChromeDriverService.CreateDefaultService();
+
+            //using (var driver = new ChromeDriver(options: chromeOptions))
+            using (var driver = new ChromeDriver(service: service, options: chromeOptions))
             {
                 INavigation nav = driver.Navigate();
-                //https://www.latamairlines.com/br/pt/oferta-voos?origin={origin}&inbound={dateTo}T10%3A00%3A00.000Z&outbound={dateFrom}T10%3A00%3A00.000Z&destination={destination}&adt=1&chd=0&inf=0&trip=RT&cabin=Economy&redemption=false&sort=RECOMMENDED""
-                nav.GoToUrl(BASE_URL.Replace("{origin}", origin).Replace("{dateTo}", dateTo).Replace("{dateFrom}",dateFrom).Replace("{destination}",destination));
+
+                var replacedUrl = "https://www.latamairlines.com/br/pt";
+                nav.GoToUrl(replacedUrl);
+                await Task.Delay(5000);
+
+                replacedUrl = BASE_URL.Replace("{origin}", origin).Replace("{dateTo}", dateTo).Replace("{dateFrom}", dateFrom).Replace("{destination}", destination);
+                //replacedUrl = "https://www.latamairlines.com/br/pt";
+                nav.GoToUrl(replacedUrl);
+
+                _logger.LogInformation($"Base URL {replacedUrl}");
 
                 _logger.LogInformation("Waiting initial page");
                 await Task.Delay(40000);
 
                 _logger.LogInformation("Select country");
-                var div = driver.FindElement(By.Id("country-suggestion-body-reject-change"));
 
-                div.Click();
+
+                IWebElement div;
+
+                try
+                {
+
+                    //var testt = driver.FindElement(By.ClassName("sec-text-container"));
+
+                    var test = driver.FindElement(By.TagName("body")).Text;
+                    var testtt = driver.FindElement(By.TagName("body"));
+
+                    div = driver.FindElement(By.Id("country-suggestion-body-reject-change"));
+                    div.Click();
+
+
+                }
+                catch (Exception)
+                {
+                    div = driver.FindElement(By.Id("country-suggestion-dialog-close"));
+                    div.Click();
+
+                }
+                finally
+                {
+                }
+
 
                 _logger.LogInformation("Select cookies policy");
                 div = driver.FindElement(By.Id("cookies-politics-button"));
@@ -84,7 +122,7 @@ namespace MyDesiredFlight.Bll.AirLines
             div = div.FindElement(By.CssSelector("button"));
 
             await Task.Delay(1000);
-            
+
             _logger.LogInformation("Button next");
             div.Click();
 
